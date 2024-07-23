b133dcc4614340e07323442d37ac23a7a3fe46e4
transmorphosis <transmorphosist@gmail.com>
2024-07-23 22:49:38 +0900

    add insert-function in array

---
+++ array/inserting-an-element-in-array.c
@@ -0,0 +1,74 @@
+/**************************************
+ * Inserting an element in an array   *
+ * Time complexity(best case): O(1)   *	
+ *                (worst case): O(n)  *
+ * Space complexity: O(n)             *
+ *************************************/
+
+#include <stdio.h>
+
+void insert(int arr[], int N, int pos, int element){
+	for(int i = N; i >= pos; i--){
+		arr[i] = arr[i-1];
+	}
+	arr[pos] = element;
+}
+
+int main(void){
+	int N;
+	scanf("%d", &N);
+
+	int i, arr[N], pos, element;
+	for(i = 0; i < N; i++){
+		scanf("%d", &arr[i]);
+	}
+	scanf("%d\n%d", &pos, &element);
+	insert(arr, N, pos, element);
+	for(i = 0; i <= N; i++){
+		printf("%d ", arr[i]);
+	}
+	return(0);
+}
+
+/* **************************
+ * Test case1:
+ * Input
+ * 5
+ * 10 20 40 50 30
+ * 0
+ * 100
+ *
+ * Expected Output
+ * 100 10 20 40 50 30
+ *
+ * Actual Output
+ * 100 10 20 40 50 30
+ *
+ * **************************
+ * Test case2:
+ * Input
+ * 10
+ * 1 5 7 8 10 12 45 67 82 13
+ * 4
+ * 100
+ * 
+ * Expected Output
+ * 1 5 7 8 100 10 12 45 67 82 13
+ * 
+ * Actual Output
+ * 1 5 7 8 100 10 12 45 67 82 13
+ * 
+ * **************************
+ * Test case3:
+ * Input
+ * 5
+ * 1 2 3 4 5
+ * 5
+ * 100
+ *
+ * Expected Output
+ * 1 2 3 4 5 100
+ *
+ * Actual Output
+ * 1 2 3 4 5 100
+ * **************************/
